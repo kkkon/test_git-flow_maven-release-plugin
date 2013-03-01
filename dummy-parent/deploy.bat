5c60401acf248005f4d0ee68da355221d7d3099f
Kiyofumi Kondoh <kiyofumi.kondoh@gmail.com>
2013-03-02 00:31:34 +0900

    use webdav

--- dummy-parent/deploy.bat
+++ dummy-parent/deploy.bat
@@ -1,1 +1,1 @@
-mvn deploy:deploy -DaltDeploymentRepository=com.github.kkkon.test::default::file:///%HOME%/content/repositories/release
+mvn --debug deploy:deploy -DaltDeploymentRepository=com.github.kkkon.test::default::dav:http://127.0.0.1:8081/content/repositories/release
